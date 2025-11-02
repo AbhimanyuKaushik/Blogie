42f6150be362fc00b00678bef1f9edcd623fb381
unknown <yashdo915@gmail.com>
2025-11-02 22:53:20 +0530

    userRoutes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -26,3 +26,22 @@ router.post("/register",async(req,res)=>{
         res.status(500).json({error:err.message});
     }
 })
+
+router.post("/login",async(req,res)=>{
+    try{
+        const{email,password} =req.body;
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message:"Invalid credentials"});
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch) return res.status(400).json({message:"Invalid credentials"});
+        const token = jwt.sign(
+            {id:user._id,username:user.username},
+            JWT_SECRET,
+            {expiresIn:"1h"}
+        );
+        
+        res.json({token,username:user.username});
+    } catch (err){
+        res.status(500).json({error:err.message});
+    }
+});
