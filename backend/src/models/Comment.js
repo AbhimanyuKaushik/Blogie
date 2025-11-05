846b8c7d6b53d2dbe39257fbedf5f6e9244d80e7
Abhimanyu kaushik <75792535+AbhimanyuKaushik@users.noreply.github.com>
2025-11-05 16:22:00 +0530

    comments and replies added

--- backend/src/models/Comment.js
+++ backend/src/models/Comment.js
@@ -15,10 +15,23 @@ const CommentSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  parentId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Comment",
+    default:null,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
+  updatedAt:{
+    type: Date,
+    default: Date.now,
+  }
 });
+
+CommentSchema.index({postId:1,createdAt:-1});
+CommentSchema.index({userId:1});
+CommentSchema.index({parentId:1});
 
 module.exports = mongoose.model("Comment", CommentSchema);
